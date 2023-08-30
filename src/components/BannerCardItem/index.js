7a9a9d5692b3479f3880d96d481b728ea70df8ba
karunakar <kamalmudiraj039@gmail.com>
2023-08-30 10:54:19 +0000

    react_reusable_banners_app

---
+++ src/components/BannerCardItem/index.js
@@ -0,0 +1,20 @@
+// Write your code here.
+import './index.css'
+
+const BannerCardItem = props => {
+  const {bannerCardsList} = props
+  const {headerText, description, className} = bannerCardsList
+  return (
+    <li className={`${className} card-container`}>
+      <div>
+        <h1 className="main-heading">{headerText}</h1>
+        <p className="description">{description}</p>
+        <button type="button" className="button">
+          Show More
+        </button>
+      </div>
+    </li>
+  )
+}
+
+export default BannerCardItem
